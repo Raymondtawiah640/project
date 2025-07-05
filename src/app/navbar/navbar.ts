dff3dc0751aec2718a46f72a2f17dad94b216334
Raymondtawiah640 <enterprisekiln@gmail.com>
2025-07-04 15:24:54 -0900

    Modified

--- src/app/navbar/navbar.ts
+++ src/app/navbar/navbar.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router'; // ✅ THIS is required for routerLink
+import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterModule, CommonModule], // ✅ NOT RouterOutlet
+  imports: [RouterModule, CommonModule],
   templateUrl: './navbar.html',
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  isMenuOpen = false;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+}
