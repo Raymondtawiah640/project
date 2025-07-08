172170254635d8f23384fe75a3a3f2016412f4ec
Raymondtawiah640 <enterprisekiln@gmail.com>
2025-07-08 09:10:49 -0900

    Added another component

--- src/app/contacts/contacts.ts
+++ src/app/contacts/contacts.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-contacts',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './contacts.html',
   styleUrl: './contacts.css'
 })
