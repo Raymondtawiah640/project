74774f92c4e8b17147e1c0cc96dbf5784d64aaf4
Raymondtawiah640 <enterprisekiln@gmail.com>
2025-08-28 11:02:22 -0900

    modified

--- src/app/about/about.ts
+++ src/app/about/about.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-about',
   standalone: true,
   imports: [],
   templateUrl: './about.html',
-  styleUrl: './about.css'
+  styleUrls: ['./about.css']
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit {
+  gistContent: string = '';
 
+  ngOnInit() {
+    fetch("https://gist.githubusercontent.com/Raymondtawiah640/6bc58a332b4186676061f8431bb0eb68/raw")
+      .then(res => res.text())
+      .then(data => {
+        this.gistContent = data;
+      });
+  }
 }
--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -8,5 +8,5 @@ import { RouterModule} from '@angular/router';
   templateUrl: './home.html',
 })
 export class HomeComponent {
-
+  
 }
